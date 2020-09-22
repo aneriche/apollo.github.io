9739b56955f68d931ac81385eca073629590fe7b
rhbrian <brianhrhee@gmail.com>
2020-09-22 15:23:59 -0700

    added tmp, new test scenes, and scene switching manager

---
+++ Apollo/Assets/Scene Manager/Loader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class Loader
+{
+
+    public enum Scene {
+        Menu,
+        Main,
+        Level1,
+    }
+
+    public static void Load(Scene scene) {
+        SceneManager.LoadScene(scene.ToString());
+    }
+}
