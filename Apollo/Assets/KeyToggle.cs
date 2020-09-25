d62aa4f72684a9b023d5b9fd5a7c02fcef2cdfaf
aneriche <aneriche@usc.edu>
2020-09-25 03:29:39 -0700

    Added Key UI object with methods to show, hide, move key

---
+++ Apollo/Assets/KeyToggle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class KeyToggle : MonoBehaviour
+{
+ 
+    public Renderer key;
+ 
+    
+    void Start() {
+        key = gameObject.GetComponentInChildren<Renderer>();  
+    }
+ 
+    public void displayKey() {
+        key.enabled = true;
+    }   
+ 
+    public void hideKey() {
+        key.enabled = false;
+    }
+ 
+    public void moveToLocation(float x,float y, float z){
+        key.transform.position = new Vector3(x,y,z);
+    }
+ 
+ 
+}
+ 
+ 
+
