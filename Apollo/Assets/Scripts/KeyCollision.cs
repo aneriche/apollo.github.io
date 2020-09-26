f28305e80088ec3f3af9d041d21d162e6bc0cf52
Urmil <urmil287@rocketmail.com>
2020-09-26 07:06:34 +0530

    initial commit of key

---
+++ Apollo/Assets/Scripts/KeyCollision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyCollision : MonoBehaviour
+{ 
+	void OnCollisionEnter(Collision other)
+ {
+            if (other.gameObject.name == "Player")
+           {
+           Debug.Log("Inside");
+                    Destroy (this.gameObject);
+           }
+ }
+}
