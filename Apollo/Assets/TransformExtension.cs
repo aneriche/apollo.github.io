6be78c1d42ad512cdfe0e326a3fed4e35107632c
ChiragSehgal1 <csehgal@usc.edu>
2020-10-04 17:50:36 -0700

    initial commit

---
+++ Apollo/Assets/TransformExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformExtension : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.position += new Vector3(0,0,Time.deltaTime*-2);
+    }
+
+  
+}
+
+
