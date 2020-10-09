5a32b38f76944b02e3b6fcc8752caa556d2285a0
rhbrian <brianhrhee@gmail.com>
2020-10-09 14:24:27 -0700

    added hallway functionality

--- Apollo/Assets/Scripts/WallCollision.cs
+++ Apollo/Assets/Scripts/WallCollision.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class collision_wall : MonoBehaviour
+public class WallCollision : MonoBehaviour
 {
     // Start is called before the first frame update
     void OnCollisionEnter(Collision col)
     {
-        if (col.collider.tag == "wallx")
+        if (col.gameObject.GetComponent<ObjectInfo>().tags[0].Equals("Despawn")) {
+            Destroy(col.collider.gameObject);
+            Destroy(col.collider.gameObject.transform.parent.gameObject);
+            Debug.Log("destroy");
+        }
+        if (col.collider.tag == "Despawn")
         {
             Destroy(col.collider.gameObject);
         }
