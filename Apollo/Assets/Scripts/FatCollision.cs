8445d2b81b9878adb3674da7578f5bcd9021b993
rhbrian <brianhrhee@gmail.com>
2020-10-16 14:42:00 -0700

    added level2 prototype

---
+++ Apollo/Assets/Scripts/FatCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FatCollision : MonoBehaviour
+{
+    void OnCollisionEnter (Collision col) {
+        if (col.collider.tag == "Key") {
+            Physics.IgnoreCollision(GetComponent<Collider>(), col.collider);
+        }
+        else if (col.collider.tag == "Door") {
+            Physics.IgnoreCollision(GetComponent<Collider>(), col.collider);
+        }
+        else if (col.collider.tag == "Transparent") {
+            Physics.IgnoreCollision(GetComponent<Collider>(), col.collider);
+            //Debug.Log("transparent");
+        }
+        else if (col.collider.tag == "RBC") {
+            Physics.IgnoreCollision(GetComponent<Collider>(), col.collider);
+        }
+    }
+}
