0403a4b6f12957473f1641eea117f91b0109d51b
Abeeku Bruce-Mensah <brucemen@usc.edu>
2020-10-20 09:20:39 -0700

    Create respawn functionality
    add deathplane prefab for plane to mark death area
    add respawn point prefab to mark where player respawns to
    create gravity script. script just moves player down -9.8 per
    frame. if player below deathplane, player moves to respawn
    point.

---
+++ Apollo/Assets/Scripts/PlayerScripts/GravityScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityScript : MonoBehaviour
+{
+    public Transform player;
+    public float gravity;
+    private CharacterController controller;
+    public Transform respawnPoint;
+    public Transform deathPlane;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!controller.isGrounded)
+        {
+            controller.Move(Vector3.down * Time.deltaTime * gravity);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (player.transform.position.y < deathPlane.transform.position.y)
+        {
+            Debug.Log(player.transform.position);
+            player.transform.position = respawnPoint.transform.position;
+        }
+    }
+}
