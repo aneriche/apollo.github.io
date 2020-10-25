d64cc82575e50d151637e84202b214a49a477ffd
rhbrian <brianhrhee@gmail.com>
2020-10-25 16:39:38 -0700

    new build, fixed colliders, added timer clock fix

--- Apollo/Assets/Scripts/TimerAnalog.cs
+++ Apollo/Assets/Scripts/TimerAnalog.cs
@@ -6,38 +6,42 @@ using UnityEngine.UI;
 public class TimerAnalog : MonoBehaviour
 {
     public float timer_duration; //time duration in seconds
+    public float timerLimit;
     public Image fillImage;
     public GameObject TimerPanel;
     private IEnumerator co;
+    private float fillValue;
 
     // Start is called before the first frame update
     void Start()
     {
         fillImage.fillAmount = 1f;
-        timer_duration = TimerPanel.GetComponent<Timer>().timeRemaining;
-        co = Timer(timer_duration);
-        StartCoroutine(co);
-
+        timerLimit = TimerPanel.GetComponent<Timer>().timeRemaining;
+        fillValue = 0;
     }
 
     void Update() {
         bool stop = TimerPanel.GetComponent<Timer>().stopTime;
-        if (stop) {
-            StopCoroutine(co);
+        timer_duration = TimerPanel.GetComponent<Timer>().timeRemaining;
+        if (!stop){
+            if (timer_duration > 0) {
+                fillValue = timer_duration / timerLimit;
+                fillImage.fillAmount = fillValue;
+            }
         }
     }
 
     // Update is called once per frame
-    public IEnumerator Timer(float duration)
+    public IEnumerator Timer()
     {
         float startTime = Time.time;
-        float time = duration;
+        float time = timer_duration;
         float value = 0;
 
-        while (Time.time - startTime < duration)
+        while (Time.time - startTime < timer_duration)
         {
             time -= Time.deltaTime;
-            value = time / duration;
+            value = time / timer_duration;
             fillImage.fillAmount = value;
             yield return null;
         }
